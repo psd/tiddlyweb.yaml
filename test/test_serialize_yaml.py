e4ea5207fdcbe502330bbaa8d33a972a39c6d6b3
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2009-12-02 13:32:14 +0000

    list_tiddlers

--- test/test_serialize_yaml.py
+++ test/test_serialize_yaml.py
@@ -7,12 +7,20 @@ Test serializing YAML
 from __future__ import absolute_import
 import yaml as pyyaml
 
+import re
+
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.recipe import Recipe
+from tiddlyweb.model.tiddler import Tiddler
 from tiddlyweb.serializer import Serializer
 
 def setup_module(module):
     module.serializer = Serializer('tiddlywebplugins.yaml')
+
+def test_list_single_recipe_as_yaml():
+    recipes = [Recipe('recipe' + str(name)) for name in xrange(1)]
+    string = serializer.list_recipes(recipes)
+    assert string == '- recipe0\n'
 
 def test_list_recipes_as_yaml():
     recipes = [Recipe('recipe' + str(name)) for name in xrange(2)]
@@ -29,4 +37,15 @@ def test_list_bags_as_yaml_unicode():
     string = serializer.list_bags(bags)
     assert string == u'- båg0\n- båg1\n'
 
-
+def test_list_tiddlers_as_yaml():
+    bag = Bag('test bag')
+    tiddlers = [Tiddler('tiddler' + str(name)) for name in xrange(2)]
+    [bag.add_tiddler(tiddler) for tiddler in tiddlers]
+    string = serializer.list_tiddlers(bag)
+    assert string.startswith(u"- bag: test bag\n")
+    assert u"\n  title: tiddler1\n" in string
+    assert u"\n  tags: []\n" in string
+    o = pyyaml.load(string)
+    assert re.match("^\d{14}$", o[0]['modified'])
+    assert o[1]['tags'] == []
+    assert o[1]['revision'] == 0
--- tiddlywebplugins/yaml.py
+++ tiddlywebplugins/yaml.py
@@ -9,6 +9,14 @@ from tiddlyweb.serializations import SerializationInterface
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.policy import Policy
 
+__version__ = "0.1"
+ 
+def init(config):
+    # register serializer
+    content_type = "text/yaml"
+    config["extension_types"]["yaml"] = content_type
+    config["serializers"][content_type] = [__name__, "text/yaml; charset=UTF-8"]
+
 class Serialization(SerializationInterface):
     """
     Access TiddlyWeb resources using the YAML representation.
@@ -32,3 +40,64 @@ class Serialization(SerializationInterface):
         Create a YAML list of bag names
         """
         return self._dump([bag.name for bag in bags])
+
+    def list_tiddlers(self, bag):
+        """
+        List the tiddlers in a bag as YAML
+        using the form described by self._tiddler_dict
+        """
+        return self._dump([self._tiddler_dict(tiddler) for
+            tiddler in bag.gen_tiddlers()])
+
+
+    # rest of this module was stolen from the json serializer, 
+    # .. maybe should be in the Serializer module? 
+    def _tiddler_dict(self, tiddler):
+        """
+        Select fields from a tiddler to create a dictonary
+        """
+        unwanted_keys = ['text', 'store']
+        wanted_keys = [attribute for attribute in tiddler.slots if
+                attribute not in unwanted_keys]
+        wanted_info = {}
+        for attribute in wanted_keys:
+            wanted_info[attribute] = getattr(tiddler, attribute, None)
+        wanted_info['permissions'] = self._tiddler_permissions(tiddler)
+        try:
+            fat = self.environ['tiddlyweb.query'].get('fat', [None])[0]
+            if fat:
+                wanted_info['text'] = tiddler.text
+        except KeyError:
+            pass # tiddlyweb.query is not there
+        return dict(wanted_info)
+
+    def _tiddler_permissions(self, tiddler):
+        """
+        Make a list of the permissions the current user has
+        on this tiddler.
+        """
+        def _read_bag_perms(environ, tiddler):
+            """
+            Read the permissions for the bag containing
+            this tiddler.
+            """
+            perms = []
+            if 'tiddlyweb.usersign' in environ:
+                store = tiddler.store
+                if store:
+                    bag = Bag(tiddler.bag)
+                    bag.skinny = True
+                    bag = store.get(bag)
+                    perms = bag.policy.user_perms(
+                            environ['tiddlyweb.usersign'])
+            return perms
+
+        bag_name = tiddler.bag
+        perms = []
+        if len(self._bag_perms_cache):
+            if bag_name in self._bag_perms_cache:
+                perms = self._bag_perms_cache[bag_name]
+            else:
+                perms = _read_bag_perms(self.environ, tiddler)
+        self._bag_perms_cache[bag_name] = perms
+        return perms
