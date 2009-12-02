30a7cca801aac65a875e460efd174da4b3f1c4d1
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2009-12-02 17:53:06 +0000

    refactored dump function to belong to the module

--- tiddlywebplugins/yaml.py
+++ tiddlywebplugins/yaml.py
@@ -5,11 +5,11 @@ YAML TiddlyWeb serializer.
 from __future__ import absolute_import
 import yaml as pyyaml
 
-from tiddlyweb.serializations import SerializationInterface
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.policy import Policy
+from tiddlyweb.serializations import SerializationInterface
 
-# hack to use the json base class for fat/skinny tiddler selection
+# temporarily use the json base class for fat/skinny tiddler selection
 from tiddlyweb.serializations.json import Serialization as SerializationInterface
 
 __version__ = "0.1"
@@ -20,6 +20,9 @@ def init(config):
     config["extension_types"]["yaml"] = content_type
     config["serializers"][content_type] = [__name__, "text/yaml; charset=UTF-8"]
 
+def dump(o):
+    return pyyaml.safe_dump(o, encoding=None, default_flow_style=False, allow_unicode=True)
+
 class Serialization(SerializationInterface):
     """
     Access TiddlyWeb resources using the YAML representation.
@@ -29,25 +32,23 @@ class Serialization(SerializationInterface):
         SerializationInterface.__init__(self, environ)
         self._bag_perms_cache = {}
 
-    def _dump(self, o):
-        return pyyaml.safe_dump(o, encoding=None, default_flow_style=False, allow_unicode=True)
-
     def list_recipes(self, recipes):
         """
         Create a YAML list of recipe names
         """
-        return self._dump([recipe.name for recipe in recipes])
+        return dump([recipe.name for recipe in recipes])
 
     def list_bags(self, bags):
         """
         Create a YAML list of bag names
         """
-        return self._dump([bag.name for bag in bags])
+        return dump([bag.name for bag in bags])
 
     def list_tiddlers(self, bag):
         """
         List the tiddlers in a bag as YAML
         using the form described by self._tiddler_dict
         """
-        return self._dump([self._tiddler_dict(tiddler) for
+        return dump([self._tiddler_dict(tiddler) for
             tiddler in bag.gen_tiddlers()])
+
